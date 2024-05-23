98f07c612f23764392419556db1a46d43707f151
Szymon423 <wachim17@gmail.com>
2024-05-23 22:43:28 +0200

    compression and encryption draft done

---
+++ compression_encryption/src/compression_encryption.cpp
@@ -0,0 +1,7 @@
+// src/communication.cpp
+#include "compression_encryption.hpp"
+#include <iostream>
+
+void initCommunication() {
+    std::cout << "Inicjalizacja modułu kompresji i enkrypcji" << std::endl;
+}
---
+++ compression_encryption/src/helper.cpp
@@ -0,0 +1,6 @@
+#include "compression_encryption.hpp"
+#include <iostream>
+
+void helperFunction() {
+    std::cout << "Funkcja pomocnicza w module kompresji i enkrypcji" << std::endl;
+}
