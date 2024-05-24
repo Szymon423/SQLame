74f773761060706a3f71f3b0bebd4c41c6516409
Szymon423 <wachim17@gmail.com>
2024-05-24 23:05:38 +0200

    generating directories system work

--- core/src/directory_management.cpp
+++ core/src/directory_management.cpp
@@ -2,30 +2,62 @@
 
 
 bool check_path_exist(fs::path path, CheckFor target) {
-    if (target == CheckFor::FILE) {
-        return fs::exists(path) && !fs::is_directory(path);
-    }
-    return fs::exists(path) && fs::is_directory(path);
+	if (target == CheckFor::FILE) {
+		return fs::exists(path) && !fs::is_directory(path);
+	}
+	return fs::exists(path) && fs::is_directory(path);
 }
 
 bool create_system_directories(fs::path file_path) {
 
-    std::ifstream json_file(file_path);
+	std::ifstream json_file(file_path);
 
-    // Sprawdź, czy plik został otwarty poprawnie
-    if (!json_file.is_open()) {
-        std::cerr << "Nie można otworzyć pliku: " << file_path << std::endl;
-        return 1;
+	// Sprawdź, czy plik został otwarty poprawnie
+	if (!json_file.is_open()) {
+		std::cerr << "Nie można otworzyć pliku: " << file_path << std::endl;
+		return 1;
+	}
+
+	// Parsuj plik JSON do obiektu nlohmann::json
+	json::json json_object;
+	json_file >> json_object;
+
+	// Zamknij plik
+	json_file.close();
+
+	fs::path base_path = json_object["base"].template get<std::string>();
+	iterate_through_directories(json_object["directories"], base_path);
+
+	return true;
+}
+
+bool create_new_directory(fs::path dir_path) {
+	if (fs::create_directory(dir_path)) {
+		std::cout << "Folder: " << dir_path << " został pomyślnie utworzony" << std::endl;
+		return true;
+	} 
+	else {
+		std::cerr << "Nie udało się utworzyć folderu: " << dir_path << std::endl;
+		return false;
+	}
+
+}
+
+void iterate_through_directories(const json::json& j, fs::path path) {
+    if (j.is_object()) {
+        for (auto it = j.begin(); it != j.end(); ++it) {
+			std::string key = it.key();
+			fs::path new_path = path / fs::path{key};
+        	std::cout << new_path << std::endl;
+			if (!check_path_exist(new_path, CheckFor::DIRECTORY)) {
+				if (!create_new_directory(new_path)) {
+					std::cout << "Nie udało się utworzyć folderu.";
+				}
+			}
+            iterate_through_directories(it.value(), new_path);
+        }
+    } 
+	else {
+        std::cout << path << std::endl;
     }
-
-    // Parsuj plik JSON do obiektu nlohmann::json
-    json::json json_object;
-    json_file >> json_object;
-
-    // Zamknij plik
-    json_file.close();
-    
-    std::cout << json_object.dump(4) << std::endl;
-
-    return true;
 }
--- core/src/directory_management.hpp
+++ core/src/directory_management.hpp
@@ -24,3 +24,13 @@ bool check_path_exist(fs::path path, CheckFor target = CheckFor::FILE);
 /// @param file_path path for JSON with configuration
 /// @return true if all went ok, false if somethong went wrong
 bool create_system_directories(fs::path file_path);
+
+/// @brief function to create directory 
+/// @param dir_path path to directory
+/// @return true if succes, false if failure
+bool create_new_directory(fs::path dir_path);
+
+/// @brief function to go through entire json and create directories based on json
+/// @param j json file
+/// @param path base path on which directories system will be generated
+void iterate_through_directories(const nlohmann::json& j, fs::path path);
